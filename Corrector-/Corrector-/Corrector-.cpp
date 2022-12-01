7673ed22b7da5096fc8ad34e086e97a0a344a2a7
CarlosAlberto251003 <carloscastelan2003@gmail.com>
2022-11-30 18:24:06 -0600

    tercer cambio

--- Corrector-/Corrector-/Corrector-.cpp
+++ Corrector-/Corrector-/Corrector-.cpp
@@ -1,7 +1,3 @@
-/*#include "corrector.h"
-#include <stdio.h>
-#include <string.h>
-
 
 #include <stdio.h>
 #include <string.h>
@@ -32,5 +28,40 @@ void main(void)	//Diccionario(char* szNombre, char szPalabras[][TAMTOKEN], int i
 		//printf("%s", aux);
 	}
 	fclose(fp);
+	printf("\n");
+	
+	FILE* fs;
+    char fg[256];
+	char aux2[100];
+
+	printf("introdusca el nombre del archivo: ");
+	scanf_s("%s", fg, 256);
+	fopen_s(&fs, fg, "r");
+	if (fs == NULL)
+	{
+		printf("no se pudo abrir el archivo");
+
+	}
+	while (!feof(fs))
+	{
+		fgets(aux2, 100, fs);
+		//printf("%s", aux2);
+	}
+	
+	
+	
+	
+	
+	
+	//fclose(fs);
+
+	//int 
+		/*if (strcmp(
+		const char* aux,
+		const char* aux2
+	));
+	*/
+
+
 
 }
